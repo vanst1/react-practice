9d70a7afc35b42cb64086f5f0ae1ccc1597898a7
Ivan Stadnytskyy <vanst1@icloud.com>
2023-02-08 12:35:07 +0200

    1-4 tasks done

--- src/App.jsx
+++ src/App.jsx
@@ -91,16 +91,18 @@ export const App = () => {
                   <i className="fas fa-search" aria-hidden="true" />
                 </span>
 
-                {searchQuery && (
+                {searchQuery.length > 0 && (
                 <span className="icon is-right">
                   {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
                   <button
                     data-cy="ClearButton"
                     type="button"
                     className="delete"
+                    onClick={() => setSearchQuery('')}
                   />
                 </span>
                 )}
+
               </p>
             </div>
 
